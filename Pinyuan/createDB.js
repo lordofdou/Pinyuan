b919dfa576e193384051e6a39432ce12eeb078ac
Haokun Xing <13218036521@163.com>
2016-06-28 15:23:39 +0800

    dir2

--- Pinyuan/createDB.js
+++ Pinyuan/createDB.js
@@ -1,13 +1,29 @@
 var mysql = require('mysql');
 var DATABASE_NAME = "pinyuan"
 var client;
+var user;
+var password;
 
-
-client = mysql.createConnection({
-	user:'root',
-	password:'123456'
+console.log("input superadmin name:");
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`data: ${chunk}`);
+  }
 });
-client.connect();
-client.query('create database if not exists '+DATABASE_NAME+' character set UTF8');
+
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
+// user = process.stdin.read();
+// console.log(user);
+// console.log("input password:");
+
+// client = mysql.createConnection({
+// 	user:'root',
+// 	password:'123456'
+// });
+// client.connect();
+// client.query('create database if not exists '+DATABASE_NAME+' character set UTF8');
 
 
