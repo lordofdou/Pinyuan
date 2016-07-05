262328128a3c6d23f12b8240a5574a78d7d2e098
Lanxuanxuan <eastblue_lan@126.com>
2016-07-05 21:27:24 +0800

    init admin_login

--- Pinyuan/routes/admin_login.js
+++ Pinyuan/routes/admin_login.js
@@ -2,9 +2,25 @@ var express = require('express');
 var router = express.Router();
 var sql = require('./sql');
 
-router.get('/',function(req,res,next){
 //get:req.query.xxx
 //post:req.body.xxx
+
+//登录页面显示
+router.get('/',function(req,res,next){
+console.log("H");
+	res.render('admin_index');
+
+});
+
+//登录
+router.post('/login', function(req, res, next){
+
+	var username = req.query.username;
+	var password = req.query.password;
+	console.log(username + " --- " + password);
+
+	// var satus = "none" || "block";
+
 });
 
 module.exports = router;
