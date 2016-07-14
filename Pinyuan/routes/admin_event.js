308e88bd2679a423d5674b826dd4903360f2b043
Haokun Xing <13218036521@163.com>
2016-07-14 10:45:13 +0800

    event_uri

--- Pinyuan/routes/admin_event.js
+++ Pinyuan/routes/admin_event.js
@@ -138,7 +138,8 @@ router.get('/delete',function(req,res,next){
     	}
 
     	//跳转到主页面
-		res.redirect("/admin_event/details?ttitle=" + title+"&id="+smallID);
+    	var uri = encodeURI("/admin_event/details?ttitle=" + title+"&id="+smallID)
+		res.redirect(uri);
     });
 });
 
